2b7377ee191b8bbd927680252e6d922da3094c25
belveloper <belhadj.abderrahmane.co@gmail.com>
2022-08-15 00:11:39 +0100

    reformatted files to match the dart hierarchy

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
-
-Future main() async{
-
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-
+    return MaterialApp(
         theme: ThemeData(
           appBarTheme: const AppBarTheme(
               backgroundColor: Colors.white,
@@ -29,13 +22,9 @@ class MyApp extends StatelessWidget {
               systemOverlayStyle: SystemUiOverlayStyle(
                   statusBarColor: Colors.white,
                   statusBarBrightness: Brightness.light,
-                  statusBarIconBrightness: Brightness.dark
-              )
-          ),
+                  statusBarIconBrightness: Brightness.dark)),
         ),
-
-        debugShowCheckedModeBanner: false, home: const FormulairePage());
+        debugShowCheckedModeBanner: false,
+        home: const FormulairePage());
   }
-
 }
-
